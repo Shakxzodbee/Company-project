939d55aee43154ee43778a8d873091471e101413
Shakxzodbee <Bakhodirovichshakhzodbek@gmail.com>
2025-11-25 05:04:42 +0500

    push

--- Token/src/App.jsx
+++ Token/src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LaunchBanner from  "./Core/Notifications/LaunchBanner.jsx";
 import CoomingSoon from "./app/layout/Coomingsoon.jsx";
 import FAQPage from "./Shared/Components/FAQ/FAQPage.jsx";
+import LogoSlider from "./Shared/Components/Slide/SlideBrand.jsx";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LaunchBanner />} />
         <Route path="/Coomingsoon" element={<CoomingSoon />} />
-        <Route path="/faq" element={<FAQPage />} />
+        <Route path="/faq" element={<LogoSlider />} />
       </Routes>
     </BrowserRouter>
   );
